e4fff02a4bc073acf636f6612031b8c387785f6a
= <=>
2024-09-23 16:50:16 +0530

    End of Day 01

--- Assigment01.py
+++ Assigment01.py
@@ -2,12 +2,18 @@ import tensorflow as tf
 import numpy as np
 from tensorflow import keras
 
+# Define the model
 model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
+
+# Compile the model
 model.compile(optimizer='sgd', loss='mean_squared_error')
 
-xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
-ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=float)
+# Define the input and output data
+xs = np.array([1, 2, 3, 4, 5, 6, 7], dtype=int)
+ys = np.array([.500 , 1.00], dtype=int)
 
+# Train the model
 model.fit(xs, ys, epochs=500)
 
-print(model.predict(np.array([10.0])))
+# Make a prediction
+print(model.predict(np.array([100])))
