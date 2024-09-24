e1a0c2661506672dae2c8bf653aa0ced34546179
= <=>
2024-09-24 13:04:08 +0530

    End Of the Mid Day 02

---
+++ Assigment03.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+print(tf.__version__)
+
+mnist = tf.keras.datasets.fashion_mnist
+
+(training_images, training_labels) ,  (test_images, test_labels) = mnist.load_data()
+
+training_images = training_images/255.0
+test_images = test_images/255.0
+
+
+model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),
+                                    tf.keras.layers.Dense(64, activation=tf.nn.relu),
+                                    tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+
+# This version has the 'flatten' removed. Replace the above with this one to see the error.
+#model = tf.keras.models.Sequential([tf.keras.layers.Dense(64, activation=tf.nn.relu),
+#                                    tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+
+
+model.compile(optimizer = 'adam',
+              loss = 'sparse_categorical_crossentropy')
+
+model.fit(training_images, training_labels, epochs=5)
+
+model.evaluate(test_images, test_labels)
+
+classifications = model.predict(test_images)
+
+print(classifications[0])
+print(test_labels[0])
---
+++ Computer_Vission.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+mnist = tf.keras.datasets.fashion_mnist
+(training_images, training_labels), (test_images, test_labels) = mnist.load_data()
+
+plt.imshow(training_images[0])
+print(training_labels[0])
+print(training_images[0])
+
+training_images  = training_images / 255.0
+test_images = test_images / 255.0
+
+model = tf.keras.models.Sequential([tf.keras.layers.Flatten(), 
+                                    tf.keras.layers.Dense(128, activation=tf.nn.relu), 
+                                    tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+model.compile(optimizer = tf.keras.optimizers.Adam(),
+              loss = 'sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(training_images, training_labels, epochs=5)
+model.evaluate(test_images, test_labels)
+classifications = model.predict(test_images)
+print(test_labels[0])
